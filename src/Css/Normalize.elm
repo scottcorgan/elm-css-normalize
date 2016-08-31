b0edac047d44ef5963bbdcf9773ea0304383d4e1
Scott Corgan <scottcorgan@gmail.com>
2016-08-31 10:59:35 -0400

    Docs

--- src/Css/Normalize.elm
+++ src/Css/Normalize.elm
@@ -1,10 +1,69 @@
-module Css.Normalize exposing (..)
+module Css.Normalize exposing (css)
+
+{-| This library is mostly a direct port of [normalize.css](https://github.com/necolas/normalize.css).
+Compile it with your elm-css code to have easier cross-browser styling experience.
+
+# Normalizing css
+@docs css
+
+-}
+
 
 import Css exposing (..)
 import Css.Elements exposing (..)
 import Css.Namespace exposing (namespace)
 
 
+{-| A [Stylesheet](https://github.com/rtfeldman/elm-css/blob/master/Tutorial.md) to rest values to make them more consistent across most browsers. You can
+include this stylesheet in your elm-css compilation file.
+
+    port module Stylesheets exposing (..)
+
+    import String
+
+    import Css exposing (..)
+    import Css.File exposing (..)
+    import Html exposing (div)
+    import Html.App as Html
+
+    import Css.Normalize
+    import My.Styles
+
+    port files : CssFileStructure -> Cmd msg
+
+
+    styles : List Css.Stylesheet
+    styles =
+        [ Css.Normalize.css
+        , My.Styles
+        ]
+
+
+    cssFiles : CssFileStructure
+    cssFiles =
+        toFileStructure [ ("dist/styles.css", compileMany styles) ]
+
+    {- Helper function to compile many stylesheets -}
+    compileMany : List Css.Stylesheet -> { warnings: List String, css: String }
+    compileMany styles =
+        let
+            results =
+                List.map Css.compile styles
+        in
+            { warnings = List.concatMap .warnings results
+            , css = String.join "\n\n" (List.map .css results)
+            }
+
+
+    main : Program Never
+    main =
+        Html.program
+            { init = ( (), files cssFiles )
+            , view = \_ -> (div [] [])
+            , update = \_ _ -> ( (), Cmd.none )
+            , subscriptions = \_ -> Sub.none
+            }
+-}
 css : Stylesheet
 css =
     (Css.stylesheet << namespace "")
